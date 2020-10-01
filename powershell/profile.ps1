a2c6ce6641d1f6c2a734c5bb1de80938d13c21e3
unknown <rschueng@gmail.com>
2020-10-01 15:19:52 -0300

    Profiles and modules

---
+++ powershell/profile.ps1
@@ -0,0 +1,8 @@
+$curUser= (Get-ChildItem Env:\USERNAME).Value
+function Prompt {
+  $ActualPath = (Get-Location).Path
+  Write-Host "$($ENV:USERNAME) in $((Get-Location).Path)" -foregroundColor "white"
+  Write-Host -NoNewline "`$" -foregroundColor "Yellow"
+  $host.UI.RawUI.WindowTitle = "$ENV:USERNAME in $ActualPath"
+  Return " "
+}
