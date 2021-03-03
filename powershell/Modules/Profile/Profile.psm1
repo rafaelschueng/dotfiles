d73dbe675cb729ed4790cb59686cafcc39f1fc45
unknown <rschueng@gmail.com>
2021-03-03 17:52:18 -0300

    Fix: Import poshgit, psreadling and new profile customization

--- powershell/Modules/Profile/Profile.psm1
+++ powershell/Modules/Profile/Profile.psm1
@@ -1,4 +1,4 @@
-function Get-HasPermissionElevated {
+function Get-HasElevatedUser {
     $Security = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent());
     return $Security.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator);
 }
--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,20 +1,30 @@
-Import-Module Profile
+if ($host.Name -eq "ConsoleHost") {
+  Import-Module Profile
+  Import-Module PSReadLine
+  Set-PSReadLineOption -PredictionSource History
+}
 
 function Prompt {
   $ActualPath = (Get-Location).Path
 
-  if(Get-HasPermissionElevated){
-    Write-Host " $($ENV:USERNAME) " -NoNewline -ForegroundColor 'red'
+  if ((Test-Path '.git')) {
+    Import-Module posh-git
+  }
+
+  if (Get-HasElevatedUser) {
+    Write-Host "[ADMIN] $($ENV:USERNAME) " -NoNewline -ForegroundColor 'red'
     $host.UI.RawUI.WindowTitle = "[Admin] $ENV:USERNAME in $ActualPath"
-  } else {
+  }
+  else {
     #When user is not elevated
     Write-Host "$($ENV:USERNAME) " -NoNewline -ForegroundColor 'green'
     $host.UI.RawUI.WindowTitle = "$ENV:USERNAME in $ActualPath"
-  }
+  } 
   
   #User type space
-  Write-Host "in $((Get-Location).Path)" -foregroundColor "white"
-  Write-Host -NoNewline "" -foregroundColor "Yellow"
+  Write-Host "in $($ActualPath)" -ForegroundColor "white" -NoNewline
+  Write-VcsStatus
+  Write-Host "`n>" -ForegroundColor "Yellow" -NoNewline
 
   Return " "
 }
